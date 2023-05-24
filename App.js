59eae6ca4229af8e6130210d841fa4e97c11bafb
gabriellepierre <gabrielle.pierre19@hotmail.fr>
2023-05-24 17:32:55 +0200

    :sparkles: Initialize layout, home screen and account screen

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout>
+      <View>
+        <StatusBar style="auto" />
+
+        {/* <View style={styles.container}>
+       
+          </View>*/}
+      </View>
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
