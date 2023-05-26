620f7af14aac636cf202e7e2b842eadfaa5e8ff9
gabriellepierre <gabrielle.pierre19@hotmail.fr>
2023-05-26 11:58:37 +0200

    :construction: Layouts

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Navigation from '../../navigation/Navigation';
+
+export default function Layout({children}:{children: ReactNode}) {
+
+    return (
+      <View style={styles.container}>
+        {children}
+        <Navigation />
+      </View>
+    )
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: '100%',
+    },
+  });
+  
+  
--- components/layouts/LightLayout.tsx
+++ components/layouts/LightLayout.tsx
@@ -1,25 +1,19 @@
 import React, { ReactNode } from 'react';
 import { StyleSheet, View } from 'react-native';
-import Navigation from '../navigation/Navigation';
 
-export default function Layout({children}:{children: ReactNode}) {
+export default function LightLayout({children}:{children: ReactNode}) {
 
     return (
       <View style={styles.container}>
         {children}
-
-        <Navigation />
-
       </View>
     )
   };
   
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      paddingHorizontal:20,
-      width: '100%',
-      backgroundColor: "#fff",
+        paddingHorizontal:20,
+        backgroundColor: '#fff',
     },
   });
   
