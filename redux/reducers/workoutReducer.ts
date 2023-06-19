9ad8cb8f607d5ff63999099c390e4596207b5a06
mchaumont <mchaumont@cikaba.com>
2023-06-19 20:23:43 +0200

    :sparkles: Add everything I forgot

--- redux/reducers/workoutReducer.ts
+++ redux/reducers/workoutReducer.ts
@@ -1,4 +1,4 @@
-import { ADD_WORKOUT, SET_WORKOUT_NAME, SET_WORKOUT_AUTHOR, SET_WORKOUT_EXERCISES } from "../constants";
+import { GET_ALL_WORKOUT, GET_WORKOUT_BY_ID, ADD_WORKOUT, UPDATE_WORKOUT, DELETE_WORKOUT, SET_WORKOUT_NAME, SET_WORKOUT_AUTHOR, SET_WORKOUT_EXERCISES } from "../constants";
 
 const initialState = {
     name: '',
@@ -7,33 +7,56 @@ const initialState = {
   };
   
 export const workoutReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case SET_WORKOUT_NAME:
-        return {
-          ...state,
-          name: action.payload,
-        };
-      case SET_WORKOUT_AUTHOR:
-        return {
-          ...state,
-          author: action.payload,
-        };
+  switch (action.type) {
+    case SET_WORKOUT_NAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case SET_WORKOUT_AUTHOR:
+      return {
+        ...state,
+        author: action.payload,
+      };
     case SET_WORKOUT_EXERCISES:
-        return {
-          ...state,
-          exercises: action.payload,
-        };
+      return {
+        ...state,
+        exercises: action.payload,
+      };
+    case GET_ALL_WORKOUT:
+      return {
+        ...state,
+        workoutList: action.payload,
+      };
+    case GET_WORKOUT_BY_ID:
+      return {
+        ...state,
+        workout: action.payload,
+      };
     case ADD_WORKOUT:
-        return {
-            ...state,
-            name: action.payload.name,
-            author: action.payload.author,
-            exercises: action.payload.exercises,
-        };
-      default:
+      return {
+        ...state,
+        id: action.payload._id,
+        name: action.payload.name,
+        author: action.payload.author,
+        exercises: action.payload.exercises,
+      };
+    case UPDATE_WORKOUT:
+      return {
+        ...state,
+        id: action.payload._id,
+        name: action.payload.name,
+        author: action.payload.author,
+        exercises: action.payload.exercises,
+      };
+    case DELETE_WORKOUT:
+      return {
+        ...state,
+      };
+    default:
         return state;
-    }
-  };
+  }
+};
   
   export default workoutReducer;
   
