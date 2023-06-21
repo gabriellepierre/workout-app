4b79bd62d9c18b8c395df7ec1d9ba2a33250a84e
Gabrielle <gabrielle.pierre19@hotmail.fr>
2023-06-21 16:35:38 +0200

    :zap: Update storage reducer

--- redux/reducers/storageReducer.ts
+++ redux/reducers/storageReducer.ts
@@ -1,5 +1,5 @@
 import { UserType } from "../../model/user/UserType";
-import { STORE_CONNECTED_USER, USER_ERROR } from "../constants";
+import { CLEAR_ALL_STORAGE, GET_CONNECTED_USER, REMOVE_CONNECTED_USER, STORE_CONNECTED_USER, USER_ERROR } from "../constants";
 
 interface StorageState {
   user: UserType | null,
@@ -14,17 +14,29 @@ const initialState: StorageState = {
 export const storageReducer = (state = initialState, action) : StorageState => {
     switch (action.type) {
         // ERROR CASE
-      case USER_ERROR: 
-        return {
-          ...state,
-          error: action.payload
-        }
+        case USER_ERROR: 
+            return {
+                ...state,
+                error: action.payload
+            }
         // ASYNC STORAGE CASES
-      case STORE_CONNECTED_USER: 
-        return {
-          ...state,
-          user: action.payload,
-        };
+        case STORE_CONNECTED_USER: 
+            return {
+                ...state,
+                user: action.payload,
+            };
+        case GET_CONNECTED_USER: 
+            return {
+                ...state,
+                user: action.payload,
+            };
+        case REMOVE_CONNECTED_USER: 
+            return {
+                ...state,
+                user: null,
+            };
+        case CLEAR_ALL_STORAGE: 
+            return initialState;
       default:
         return state;
       }
